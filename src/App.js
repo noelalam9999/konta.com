af3102309a8849815629768ecabb37b27477cd71
fahimfayaz <fahimfayaz18@gmail.com>
2020-08-18 00:54:53 +0600

    userprofile page added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import { setContext } from "apollo-link-context";
-import Form, { Product_upload } from "./Products/Product_upload";
+import Product_upload from "./Products/Product_upload";
 import Switch from 'react-bootstrap/esm/Switch';
 import { Route, BrowserRouter } from 'react-router-dom';
 import PostList from "./Products/Product_list"
 import {Search} from './Search/Search';
+import userprofile from './User/userprofile';
+
 
 function App() {
   const httpLink = new HttpLink({
@@ -26,12 +28,14 @@ function App() {
     <ApolloProvider client={client}>
         
            <Switch>
-             <Route exact path='/' component={LandingPage}/>
-<Route exact path='/product_upload' component={Product_upload} />
-<Route exact path='/products' component={PostList} />
-<Route path='/search' component={Search}/>
 
-</Switch>
+              <Route exact path='/' component={LandingPage}/>
+              <Route exact path='/product_upload' component={Product_upload} />
+              <Route exact path='/products' component={PostList} />
+              <Route path='/search' component={Search}/>
+              <Route path='/User/userprofile' component={userprofile}/>
+
+            </Switch>
            
     </ApolloProvider>
     </BrowserRouter>
